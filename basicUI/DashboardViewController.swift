39994d3baaf13ed35cc774b7fcc5d604e2bd9d89
Manikanta <mmanikanta0007@gmail.com>
2021-05-08 03:22:19 +0530

    Dashboard UI

--- basicUI/DashboardViewController.swift
+++ basicUI/DashboardViewController.swift
@@ -9,22 +9,25 @@
 import UIKit
 
 class DashboardViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        viewWillDisappear(true)
+        let backgroundImage = UIImage.init(named: "LaunchScreen-AspectFill.png")
+            let backgroundImageView = UIImageView.init(frame: self.view.frame)
+
+            backgroundImageView.image = backgroundImage
+        backgroundImageView.contentMode = .scaleToFill
+
+            self.view.insertSubview(backgroundImageView, at: 0)
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: animated)
     }
-    */
 
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
 }
--- basicUI/ViewController.swift
+++ basicUI/ViewController.swift
@@ -11,8 +11,19 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        viewWillDisappear(true)
         // Do any additional setup after loading the view.
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    navigationController?.setNavigationBarHidden(true, animated: animated)
+}
+
+override func viewWillDisappear(_ animated: Bool) {
+    super.viewWillDisappear(animated)
+    navigationController?.setNavigationBarHidden(false, animated: animated)
+}
 
 
 }
