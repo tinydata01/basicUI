11d267b78e4e2b746af5a7af8dbaabf9008f03ad
Manikanta <mmanikanta0007@gmail.com>
2021-05-07 20:05:28 +0530

    Basic UI Skeleton

--- basicUI/ViewController.swift
+++ basicUI/ViewController.swift
@@ -1,37 +1,19 @@
 //
 //  ViewController.swift
-//  basicUI
+//  skeleton
 //
-//  Created by user187847 on 4/5/21.
-//  Copyright © 2021 user187847. All rights reserved.
+//  Created by user189922 on 5/7/21.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet var _password: UITextField!
-    @IBOutlet var _email: UITextField!
-    
-    @IBOutlet var _loginButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        let emailImage = UIImage(named:"mail")
-        addLeftImageTo(txtField: _email, andImage: emailImage!)
-        
-        let passwordImage = UIImage(named:"password")
-        addLeftImageTo(txtField: _password, andImage: passwordImage!)
-
-        
     }
 
-    func addLeftImageTo(txtField:UITextField, andImage img: UIImage){
-        let leftImageView = UIImageView(frame: CGRect(x: 0, y: 0, width: img.size.width, height: img.size.height))
-        leftImageView.image = img
-        txtField.leftView = leftImageView
-        txtField.leftViewMode = .always
-    }
-    
+
 }
 
