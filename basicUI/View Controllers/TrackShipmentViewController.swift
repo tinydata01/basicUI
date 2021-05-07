d19a4a38e162871e4d3abaea6ecf1e87877b7aa3
Manikanta <mmanikanta0007@gmail.com>
2021-05-07 20:52:31 +0530

    Added View controllers

---
+++ basicUI/View Controllers/TrackShipmentViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TrackShipmentViewController.swift
+//  basicUI
+//
+//  Created by user189922 on 5/7/21.
+//  Copyright © 2021 user187847. All rights reserved.
+//
+
+import UIKit
+
+class TrackShipmentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
