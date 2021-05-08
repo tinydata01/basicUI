34bd5634820222a102e03bf07b4fafb0c8feb1e3
Manikanta <mmanikanta0007@gmail.com>
2021-05-08 13:45:30 +0530

    Vessel scdelude header

--- basicUI/VesselScheduleViewController.swift
+++ basicUI/VesselScheduleViewController.swift
@@ -12,8 +12,18 @@ class VesselScheduleViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        viewWillDisappear(true)
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: animated)
     }
     
 
