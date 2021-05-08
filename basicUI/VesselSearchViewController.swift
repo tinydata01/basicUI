798015cbc70f69ac70841ce5bac991e5a6b44134
krishna <krishna@divami.com>
2021-05-08 17:41:06 +0530

    search bar implemented

--- basicUI/VesselSearchViewController.swift
+++ basicUI/VesselSearchViewController.swift
@@ -7,13 +7,21 @@
 //
 
 import UIKit
-
+import DropDown
 class VesselSearchViewController: UIViewController,UITextFieldDelegate  {
 
+    @IBOutlet weak var hiddenView: UIView!
     @IBOutlet weak var searchField: UITextField!
     @IBAction func trackButton(_ sender: Any) {
         print("ok")
     }
+    let dropDown = DropDown()
+    var jon =
+[["firstName" : "John Sam","lastName" : "Smith"],
+["firstName" : "Johnny","lastName" : "Smith"],
+["firstName" : "Albert","lastName" : "Smith"],
+["firstName" : "Alby","lastName" : "Wright"],
+["firstName" : "Smith","lastName" : "Green"]]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,6 +33,13 @@ class VesselSearchViewController: UIViewController,UITextFieldDelegate  {
         searchField.leftViewMode = UITextField.ViewMode.always
         searchField.attributedPlaceholder = NSAttributedString(string: "Search by BL, BCG#, container#",attributes: [NSAttributedString.Key.foregroundColor: UIColor.black])
         viewWillDisappear(true)
+        dropDown.anchorView = hiddenView
+        dropDown.bottomOffset = CGPoint(x: 0, y:(dropDown.anchorView?.plainView.bounds.height)!)
+        dropDown.topOffset = CGPoint(x: 0, y:-(dropDown.anchorView?.plainView.bounds.height)!)
+        dropDown.selectionAction = { [unowned self] (index: Int, item: String) in
+  print("Selected item: \(item) at index: \(index)")
+            self.searchField.text = item
+}
     }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -38,6 +53,18 @@ class VesselSearchViewController: UIViewController,UITextFieldDelegate  {
     
         func textFieldShouldReturn(_ textField: UITextField) -> Bool {
             searchField.resignFirstResponder()
+            if(searchField.text?.count ?? 0 >= 2){
+                let txt = searchField.text ?? ""
+                let range = txt.index(txt.startIndex, offsetBy: 2)
+                let srch = txt[..<range]
+                print(srch)
+                let filtered = jon.filter {
+    return $0["firstName"]?.range(of: srch, options: .caseInsensitive) != nil
+}
+                print(filtered)
+                dropDown.dataSource = filtered.compactMap { $0["firstName"] }
+                dropDown.show()
+            }
             /*let vc = self.storyboard?.instantiateViewController(identifier: "tempViewController") as! tempViewController
             self.navigationController?.pushViewController(vc, animated: true)*/
             
