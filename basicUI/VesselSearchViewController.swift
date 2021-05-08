eb9e917120c45f4383b4650a739fe1f3f8d7a9c3
krishna <krishna@divami.com>
2021-05-08 13:12:18 +0530

    vessel search screen added

---
+++ basicUI/VesselSearchViewController.swift
@@ -0,0 +1,30 @@
+//
+//  VesselSearchViewController.swift
+//  basicUI
+//
+//  Created by user189958 on 5/8/21.
+//  Copyright © 2021 user187847. All rights reserved.
+//
+
+import UIKit
+
+class VesselSearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
