4968f557f5a8e4000a1c15e3e2b74b9cc8b115e3
krishna <krishna@divami.com>
2021-05-09 20:02:12 +0530

    width changed

--- basicUI/ViewController.swift
+++ basicUI/ViewController.swift
@@ -105,7 +105,7 @@ func setIcon(_ image: UIImage) {
                   CGRect(x: 10, y: 5, width: 20, height: 20))
    iconView.image = image
    let iconContainerView: UIView = UIView(frame:
-                  CGRect(x: 20, y: 0, width: 30, height: 30))
+                  CGRect(x: 20, y: 0, width: 50, height: 30))
    iconContainerView.addSubview(iconView)
    leftView = iconContainerView
    leftViewMode = .always
